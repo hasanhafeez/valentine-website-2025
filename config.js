35b4ec5e167b8f26e8b814437815bc45f6800c8b
hasanhafeez <hasanhafeez222@gmail.com>
2026-02-09 16:14:54 -0800

    Update valentineName and celebration messages

--- config.js
+++ config.js
@@ -4,8 +4,8 @@
 
 const CONFIG = {
     // Your Valentine's name that will appear in the title
-    // Example: "Jade", "Sarah", "Mike"
-    valentineName: "Jade",
+   
+    valentineName: "Amu🥺",
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
@@ -15,7 +15,7 @@ const CONFIG = {
     // Find more emojis at: https://emojipedia.org
     floatingEmojis: {
         hearts: ['❤️', '💖', '💝', '💗', '💓'],  // Heart emojis
-        bears: ['🧸', '🐻']                       // Cute bear emojis
+        bears: ['🧸', '🐻','🥺']                       // Cute bear emojis
     },
 
     // Questions and answers
@@ -42,15 +42,15 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
+        extreme: "HEHEHEHE! You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
         high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
         normal: "And beyond! 🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        title: "Yayayaya hehe! I'm the luckiest person in the world MUAHH! 🎉💝💖💝💓",
+        message: "Now come get your note, my kisses and lots of lots of love from me hehe!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
